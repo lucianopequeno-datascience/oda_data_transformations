3ac414f825edc2177ca11d3e041e1d74e4535cd6
Luciano Pequeno <lucianopequenodossantos@gmail.com>
2026-08-12 19:54:18 +0000

    Adição de funções macros JavaScript para padronizar tratamento de bases onde as colunas apresentam valores padronizados pelos sistemas de origem

---
+++ includes/csSexoTreatment.js
@@ -0,0 +1,12 @@
+/// função para de tratar CS_SEXO nas bases do Sinan
+
+function csSexoTreatmenet(coluna) {
+  return `
+    CASE
+      WHEN ${coluna} = 'F' THEN 'Feminino'
+      WHEN ${coluna} = 'M' THEN 'Masculino'
+      ELSE 'Ignorado'
+    END
+  `;
+}
+module.exports = {csSexoTreatmenet};
